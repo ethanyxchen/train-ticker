b00a8f42b1b889550791bfec5a00f634d2901683
Ethan Chen <ethanyxchen@berkeley.edu>
2026-06-16 17:07:36 +0100

    Use one slower split flap timing

--- components/split-flap-text.tsx
+++ components/split-flap-text.tsx
@@ -54,8 +54,7 @@ const splitFlapStyle = {
 } satisfies SplitFlapStyle;
 
 const splitFlapCharacters = [...SPLIT_FLAP_CHARACTERS];
-const SPLIT_FLAP_TIMING_MS = 56;
-const SPINNING_SPLIT_FLAP_TIMING_MS = SPLIT_FLAP_TIMING_MS * 2;
+const SPLIT_FLAP_TIMING_MS = 112;
 const REPLAY_SETTLE_MS = 70;
 const ANIMATION_BUFFER_MS = 140;
 
@@ -256,7 +255,7 @@ export function SplitFlapText({
         value: getSplitFlapValueAfterSteps(paddedValue, step),
         version: step,
       });
-    }, SPINNING_SPLIT_FLAP_TIMING_MS);
+    }, SPLIT_FLAP_TIMING_MS);
 
     return () => {
       window.clearInterval(intervalId);
