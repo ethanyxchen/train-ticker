8d277986d1278db0d1c00d12ae24c448fb45ee81
Ethan Chen <ethanyxchen@berkeley.edu>
2026-06-16 16:55:46 +0100

    Slow split flap animation speed

--- components/split-flap-text.tsx
+++ components/split-flap-text.tsx
@@ -54,7 +54,7 @@ const splitFlapStyle = {
 } satisfies SplitFlapStyle;
 
 const splitFlapCharacters = [...SPLIT_FLAP_CHARACTERS];
-const SPLIT_FLAP_TIMING_MS = 28;
+const SPLIT_FLAP_TIMING_MS = 56;
 const REPLAY_SETTLE_MS = 70;
 const ANIMATION_BUFFER_MS = 140;
 
